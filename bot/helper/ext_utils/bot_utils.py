59c6350c1f3d4da5f0e2d105c917cbc1e77b04d2
Magneto261299 <58435505+Magneto261299@users.noreply.github.com>
2020-01-18 22:44:16 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -21,7 +21,7 @@ class MirrorStatus:
 
 
 PROGRESS_MAX_SIZE = 100 // 8
-PROGRESS_INCOMPLETE = ['▏', '▎', '▍', '▌', '▋', '▊', '▉']
+PROGRESS_INCOMPLETE = ['●', '●', '●', '●', '●', '●', '●']
 
 SIZE_UNITS = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
 
@@ -50,6 +50,7 @@ def get_readable_file_size(size_in_bytes) -> str:
         size_in_bytes /= 1024
         index += 1
     try:
+        return f'{round(size_in_bytes, 2)} {SIZE_UNITS[index]}'
         return f'{round(size_in_bytes, 2)}{SIZE_UNITS[index]}'
     except IndexError:
         return 'File too large'
@@ -65,10 +66,10 @@ def get_progress_bar_string(status):
     p = min(max(p, 0), 100)
     cFull = p // 8
     cPart = p % 8 - 1
-    p_str = '█' * cFull
+    p_str = '●' * cFull
     if cPart >= 0:
         p_str += PROGRESS_INCOMPLETE[cPart]
-    p_str += ' ' * (PROGRESS_MAX_SIZE - cFull)
+    p_str += '○' * (PROGRESS_MAX_SIZE - cFull)
     p_str = f"[{p_str}]"
     return p_str
 
@@ -85,12 +86,17 @@ def get_readable_message():
     with download_dict_lock:
         msg = ""
         for download in list(download_dict.values()):
-            msg += f"<i>{download.name()}</i> - "
-            msg += download.status()
+            msg += f"<b>Name:</b> <code>{download.name()}</code> "
+            msg += f"\n<b>Status:</b> <code>{download.status()}</code>"
             if download.status() != MirrorStatus.STATUS_ARCHIVING:
                 msg += f"\n<code>{get_progress_bar_string(download)} {download.progress()}</code> of " \
-                    f"{download.size()}" \
-                    f" at {download.speed()}, ETA: {download.eta()}\n\n"
+                    f"<code>{download.size()}</code>" \
+                    f"\n<b>Speed:</b> <code>{download.speed()}</code>   <b>ETA:</b> <code>{download.eta()}</code> "
+            if download.status() == MirrorStatus.STATUS_DOWNLOADING:
+                if hasattr(download, 'is_torrent'):
+                    msg += f"\n<b>Info:</b> <code>Seeders: {download.download().num_seeders}</code>" \
+                           f"   <code>Peers: {download.download().connections}</code>"
+            msg += "\n\n"
         return msg
 
 
