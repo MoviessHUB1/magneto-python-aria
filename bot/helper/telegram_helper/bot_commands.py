6b55b18f5f1d2b50ef519222852b20f5a1c1e3c6
magneto261290 <54003800+magneto261290@users.noreply.github.com>
2020-01-30 23:22:18 +0530

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -1,18 +1,18 @@
 class _BotCommands:
     def __init__(self):
         self.StartCommand = 'start'
-        self.MirrorCommand = 'krbc'
-        self.TarMirrorCommand = 'zipkrbc'
-        self.CancelMirror = 'hat'
-        self.CancelAllCommand = 'hatbsdk'
-        self.ListCommand = 'de'
-        self.StatusCommand = 'dikha'
+        self.MirrorCommand = 'mirror'
+        self.TarMirrorCommand = 'tarmirror'
+        self.CancelMirror = 'cancel'
+        self.CancelAllCommand = 'cancelall'
+        self.ListCommand = 'list'
+        self.StatusCommand = 'status'
         self.AuthorizeCommand = 'authorize'
         self.UnAuthorizeCommand = 'unauthorize'
         self.PingCommand = 'ping'
-        self.StatsCommand = 'gaand_dikha'
+        self.StatsCommand = 'stats'
         self.HelpCommand = 'help'
         self.LogCommand = 'log'
-
+        
 
 BotCommands = _BotCommands()
